ae1103ac03b8beb1400a2d9a35f3daa7a63eef8c
Adam Cottrill <racottrill@gmail.com>
2018-10-26 16:41:46 -0400

    Basic filtering functionality has been implemented
    Basic filter functionality has been implemented, but the options are
    hard coded and the same for all views. Up next - make it dynamic.

--- tickets/templatetags/ticket_extras.py
+++ tickets/templatetags/ticket_extras.py
@@ -1,12 +1,9 @@
-# from django import template
+
 from django import template
 from django.template.defaultfilters import stringfilter
-#from django.contrib.auth import user
 from django.utils.safestring import mark_safe
 
-
 register = template.Library()
-
 
 
 @register.filter
@@ -21,16 +18,15 @@ def priority_btn(priority, btn_size="xs"):
         '2': ['warning', 'High'],
         '3': ['success', 'Normal'],
         '4': ['info', 'Low'],
-        '5': ['default', 'Very Low'],        
+        '5': ['default', 'Very Low'],
     }
 
-    #import pdb; pdb.set_trace()
-    
-    btn_attr = btn_map.get(priority)        
+    btn_attr = btn_map.get(priority)
     btn = '<button type="button" class="btn btn-{0} btn-{1}">{2}</button>'
     btn = btn.format(btn_attr[0], btn_size, btn_attr[1])
 
-    return mark_safe(btn)    
+    return mark_safe(btn)
+
 
 @register.filter
 @stringfilter
@@ -40,42 +36,53 @@ def status_btn(status, btn_size='xs'):
     '''
 
     btn_map = {
-        'new':['success', 'New'],
-        'accepted':['info', 'Accepted'],
-        'assigned':['primary', 'Assigned'],
-        'reopened':['warning', 'Reopened'],
-        'closed':['default', 'Closed'],
-        'duplicate':['default', 'Closed - Duplicate'],
-        'split':['default', 'Closed - Split'],                   
+        'new': ['success', 'New'],
+        'accepted': ['info', 'Accepted'],
+        'assigned': ['primary', 'Assigned'],
+        'reopened': ['warning', 'Reopened'],
+        'closed': ['default', 'Closed'],
+        'duplicate': ['default', 'Closed - Duplicate'],
+        'split': ['default', 'Closed - Split'],
     }
 
     btn_attr = btn_map.get(status.lower(), ['default', status])
-    
+
     if btn_size == 'lg' and status in ('closed', 'duplicate', 'split'):
-        btn_attr[0]='danger'
+        btn_attr[0] = 'danger'
 
     btn = '<button type="button" class="btn btn-{0} btn-{1}">{2}</button>'
     btn = btn.format(btn_attr[0],  btn_size, btn_attr[1])
 
     return mark_safe(btn)
-    
+
+
 @register.filter
 @stringfilter
 def ticket_type_btn(ticket_type, btn_size="xs"):
     '''given a ticket type and button size, return a colour-coded
     bootstrap button with an appropriate label.
     '''
-    
+
     btn_map = {
         'bug': ['danger', 'Bug Report'],
         'feature': ['default', 'Feature Request'],
     }
-    
-    btn_attr = btn_map.get(ticket_type.lower())        
+
+    btn_attr = btn_map.get(ticket_type.lower())
     btn = '<button type="button" class="btn btn-{0} btn-{1}">{2}</button>'
     btn = btn.format(btn_attr[0],  btn_size, btn_attr[1])
 
-    return mark_safe(btn)    
+    return mark_safe(btn)
 
 
-        
+@register.filter
+@stringfilter
+def classify(ticket_attribute):
+    '''Given a ticket attribute, return a string representation that could
+    be used to to assing a class in an html template.
+
+    '''
+
+    classString = ticket_attribute.replace(' ', '-').lower()
+
+    return mark_safe(classString)
