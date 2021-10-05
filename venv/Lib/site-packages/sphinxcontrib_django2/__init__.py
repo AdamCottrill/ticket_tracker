77aaf013461e6a1c6d78de5a1f80f2667267722b
VickiDerbyshire <v.derbyshire@hotmail.com>
2021-10-05 13:06:24 -0400

    Adam's changes 10/05
    new changes by adam

---
+++ venv/Lib/site-packages/sphinxcontrib_django2/__init__.py
@@ -0,0 +1,26 @@
+"""
+This is a sphinx extension which improves the documentation of Django apps.
+"""
+__version__ = "1.1.1"
+
+from . import docstrings, roles
+
+
+def setup(app):
+    """
+    Allow this module to be used as sphinx extension.
+
+    Setup the two sub-extensions :mod:`~sphinxcontrib_django2.docstrings` and
+    :mod:`~sphinxcontrib_django2.roles` which can also be imported separately.
+
+    :param app: The Sphinx application object
+    :type app: ~sphinx.application.Sphinx
+    """
+    docstrings.setup(app)
+    roles.setup(app)
+
+    return {
+        "version:": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
